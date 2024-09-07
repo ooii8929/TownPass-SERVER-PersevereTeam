e8f2c6abc13a5083c0926e230a288355a9c1f903
vera.zhen63 <vera.zhen63@gmail.com>
2024-09-07 19:29:13 +0800

    fix interface

--- websrv/src/example_for_models.py
+++ websrv/src/example_for_models.py
@@ -1,12 +1,11 @@
-from models.bot_model import Bot, Language, Preference, Character, SectionStage
+from models.bot_model import Bot, Language, Style, SectionStage
 
 def main():
     # 创建一个 Bot 实例
     my_bot = Bot(
         language=Language.CHINESE,
-        preference=Preference.HISTORY,
         age=10,
-        character=Character.HAPPY,
+        style=Style.HAPPY,
         user_location='永樂市場',
         all_locations = ["大稻埕碼頭", "龍山寺", "中正紀念堂", "九份老街","十分瀑布", "松山文創園區", "北投溫泉", "淡水老街"],
         visited_locations = ["大稻埕碼頭", "龍山寺", "淡水老街"]
--- websrv/src/models/bot_model.py
+++ websrv/src/models/bot_model.py
@@ -5,12 +5,7 @@ class Language(Enum):
     ENGLISH = auto()
 # 待新增
 
-class Preference(Enum):
-    HISTORY = auto()
-    NATURE = auto()
-# 待新增
-
-class Character(Enum):
+class Style(Enum):
     HAPPY = auto()
     DISGUST = auto()
 # 待新增
@@ -29,11 +24,10 @@ class QestionType(Enum):
 
 
 class Bot:
-    def __init__(self, language: Language, preference: Preference, age: int, character: Character, user_location: str,all_locations: list[str],visited_locations:list[str]):
+    def __init__(self, language: Language, age: int, style: Style, user_location: str,all_locations: list[str],visited_locations:list[str]):
         self.language = language
-        self.preference = preference
         self.age = age
-        self.character = character
+        self.style = style
         self.user_location = user_location
         self.all_locations = all_locations
         self.visited_locations = visited_locations
@@ -44,7 +38,7 @@ class Bot:
         res_data = {
             "content": "嗨，小朋友們！我們今天要來認識一個非常有趣的地方喔！...",
             "question": "好了，小朋友們，現在我們來玩個小遊戲吧！我有一個問題要問你們：大稻埕碼頭最有名的出口商品是什麼呢？",
-            "type": "option",
+            "type": QestionType.OPTION,
              "options": [
                 {"label": "A", "option": "糖果"},
                 {"label": "B", "option": "茶葉"},
@@ -56,14 +50,14 @@ class Bot:
         # res_data = {
         #     "content": "你的回答正確喔！...",
         #     "question": "還想問你另外一個問題是...",
-        #     "type": "open",
+        #     "type": QestionType.OPEN,
         #     "want_more": True
         # }
         # sectionStage: End, inputText: "我覺得大稻埕很好玩"
         # res_data = {
         #     "content": "感謝你的分享...",
         #     "question": "接下來想推薦給你一些值得去的其他地方...",
-        #     "type": "next",
+        #     "type": QestionType.NEXT,
         #     "want_more": False
         # }
         return res_data
